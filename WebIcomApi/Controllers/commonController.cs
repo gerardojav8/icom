e5f8bd9adc9f523915657f898e754584cac756e3
jav8586 <gerardo_jav_8@hotmail.com>
2016-10-10 14:11:34 -0500

    update

--- WebIcomApi/Controllers/commonController.cs
+++ WebIcomApi/Controllers/commonController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DAOicom;
+using DAOicom.Helpers;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -29,6 +32,57 @@ namespace WebIcomApi.Controllers
                 objfechho.Fecha = strfecha;
 
                 return objfechho;
+            }
+            catch (Exception e)
+            {
+                clsError objerr = new clsError();
+                objerr.error = "No se ha podido obtener la fecha y hora actual";
+                objerr.result = 0;
+                return objerr;
+            }
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("cambiarPass")]
+        public Object cambiarPass(JObject json)
+        {
+            try
+            {
+                String idusuario = json["idusuario"].ToString();
+                String newpass = json["newpass"].ToString();
+                String oldpass = json["oldpass"].ToString();
+
+                usuariosHelper ushelp = new usuariosHelper();
+                usuarios us = ushelp.getUsuarioByID(Int32.Parse(idusuario));
+
+                if (us == null) {
+                    clsError objerr = new clsError();
+                    objerr.error = "No se ha podido obtener los datos para el usuario";
+                    objerr.result = 1;
+                    return objerr;
+                }
+
+                if (!us.passapp.ToLower().Equals(oldpass.ToLower())) {
+                    clsError objerr = new clsError();
+                    objerr.error = "El password anterior no coincide, verifiquelo por favor";
+                    objerr.result = 1;
+                    return objerr;
+                }
+
+                us.passapp = newpass;
+
+                String resp = ushelp.updateUsuario(us);
+                if (!resp.Equals("")) {
+                    clsError objerr = new clsError();
+                    objerr.error = "Ha existido un error al momento de modificar el password";
+                    objerr.result = 1;
+                    return objerr;
+                }
+
+                Dictionary<String, String> dicresp = new Dictionary<string, string>();
+                dicresp.Add("respuesta", "cambio de password exitoso");
+                return dicresp;
             }
             catch (Exception e)
             {
